1cd4ead37df8303a7cf7211f11173cd81acbf346
Aron7R <aronmassuud7@gmail.com>
2025-03-22 21:59:07 +0600

    Add files via upload

---
+++ Lesson5.js
@@ -0,0 +1,39 @@
+// LESSON 1
+var name = "Alex"
+var name2= "Jordan"
+name="Michael"
+
+let number = "10"
+number="100"
+
+const value = 200
+
+
+// LESSON 2
+var NAME = "  ARON MASSUUD"
+var AGE = "12"
+var CLASS = "6"
+
+
+console.log("Hello I am a human , My name is "+  NAME );
+
+
+
+// LESSON 3
+var a = 5;
+var b = 7;
+var c = a + b;
+
+// Arithmetic
+console.log(c);
+console.log(a / b);
+console.log(a * b);
+console.log(a - b);
+
+// Logical
+console.log(a > b);
+console.log(a < b);
+console.log(a == b);
+console.log(a!==b)
+
+
