e3eda5d14afdf522b00ba5723c18fb48f33e9870
Aron7R <aronmassuud7@gmail.com>
2025-05-03 21:56:27 +0600

    Add files via upload

---
+++ Lesson2.py
@@ -0,0 +1,55 @@
+#Data Type - String
+Name="Aron"
+Country="Bangladesh"
+School="Academia"
+
+#Integer Data Type
+a=1
+b=1
+c=1
+
+#Float (Integer with Decimal Point)
+d=1.2
+e=2.4
+f=3.6
+
+#Boolean (True or False)
+Math_pass= True
+a=False
+b=True
+
+#Operators
+a=2
+b=3
+
+#Arithmetic Operator
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+
+#Logical Operator
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+
+text = "Hello"
+
+# Index
+print(text[0])
+print(text[1])
+print(text[2])
+print(text[3])
+print(text[4])
+
+# Inbuilt Functions
+print(text.upper())
+print(text.lower())
+print(text.swapcase())
+print(text.find("o"))
+print(text.find(H))
